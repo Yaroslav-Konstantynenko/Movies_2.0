7c3b3936a64be916ca827577343dc68cb9ca0b37
CaspeR <konstantynenko.yaroslav@gmail.com>
2024-02-08 16:18:40 +0200

    first commit

---
+++ Movies 2.0/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Movies 2.0
+//
+//  Created by Yaroslav Konstantynenko on 08.02.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+    
+}
